aadc0f90e876b524c155f8a6d8de9e9c8cd4210b
Lucas Godoi <luscagodoi@gmail.com>
2026-04-13 23:20:51 -0300

    Changed "turma" instantiation
    Removed duplicate Turma instantiation in main method.

--- EX02/br/inatel/poo/Main.java
+++ EX02/br/inatel/poo/Main.java
@@ -5,11 +5,11 @@ import br.inatel.poo.turmas.Turma;
 
 public class Main {
     public static void main(String[] args) {
+        Turma turma = new Turma();
+        
         Aluno aluno1 = new Aluno("Sandro", 550, new double[]{16, 12, 14});
         Aluno aluno2 = new Aluno("Gabao", 888, new double[]{13, 15, 15, 9});
         Aluno aluno3 = new Aluno("Bruna", 1010, new double[]{17, 14, 14, 11, 10});
-
-        Turma turma = new Turma();
 
         turma.adicionarAluno(aluno1);
         turma.adicionarAluno(aluno2);
